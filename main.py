6d8d718483fd29d86bf9aa6cd8cd48325fc6b57a
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-13 18:42:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
-import os, cv2, pydicom
+import os 
+import cv2 as cv
+import pydicom
 import matplotlib.pyplot as plt
 import skimage.segmentation as seg
 from PIL import Image
@@ -20,8 +22,8 @@ def ShowImage(title,img,ctype):
 def masking(image):
     foreground_value = 255
     mask = np.uint8(image == foreground_value)
-    labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
-    largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
+    labels, stats = cv.connectedComponentsWithStats(mask, 4)[1:3]
+    largest_label = 1 + np.argmax(stats[1:, cv.CC_STAT_AREA])
     image = np.zeros_like(image)
     image[labels == largest_label] = foreground_value
     ShowImage('masking',image,'rgb')
@@ -39,32 +41,32 @@ st.image(hasil, caption='Gambar Origin',use_column_width=True)
 
 
 #OTSU THRESHOLDING
-_,binarized = cv2.threshold(hasil, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+_,binarized = cv.threshold(hasil, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
 foreground_value = 255
 mask = np.uint8(binarized == foreground_value)
-labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
-largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
+labels, stats = cv.connectedComponentsWithStats(mask, 4)[1:3]
+largest_label = 1 + np.argmax(stats[1:, cv.CC_STAT_AREA])
 binarized = np.zeros_like(binarized)
 binarized[labels == largest_label] = foreground_value
 st.image(binarized, caption='Otsu Image',use_column_width=True)
 
 # erosion from otsu
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-erosion = cv2.erode(binarized,kernel,iterations = 4)
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3))
+erosion = cv.erode(binarized,kernel,iterations = 4)
 foreground_value = 255
 mask = np.uint8(erosion == foreground_value)
-labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
-largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
+labels, stats = cv.connectedComponentsWithStats(mask, 4)[1:3]
+largest_label = 1 + np.argmax(stats[1:, cv.CC_STAT_AREA])
 erosion = np.zeros_like(erosion)
 erosion[labels == largest_label] = foreground_value
 st.image(erosion, caption='Erosion Image',use_column_width=True)
 
 # dilation from opening
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-dilasi = cv2.dilate(erosion,kernel,iterations = 2)
+kernel = cv.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+dilasi = cv.dilate(erosion,kernel,iterations = 2)
 foreground_value = 255
 mask = np.uint8(dilasi == foreground_value)
-labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
+labels, stats = cv.connectedComponentsWithStats(mask, 4)[1:3]
 largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
 dilasi = np.zeros_like(dilasi)
 dilasi[labels == largest_label] = foreground_value
