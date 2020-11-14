d3f42f570035b21c2231e230bfca912757d6d5ce
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-14 08:49:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ Berikut ini algoritma yang digunakan untuk Segmentasi Otak
 IMAGE_PATHS = os.listdir("dicom")
 option = st.sidebar.selectbox('Pilih File Dicom?',IMAGE_PATHS)
 st.sidebar.write('You selected:', option)
-morphology1 = st.sidebar.checkbox('Morphology1')
-morphology2 = st.sidebar.checkbox('Morphology2')
-if morphology1:
+bukadata = st.sidebar.checkbox('Open Data')
+otsuthreshold = st.sidebar.checkbox('Otsu Threshold')
+if bukadata:
     # get the data
     d = pydicom.read_file('dicom/'+option)
     file = np.array(d.pixel_array)
@@ -28,6 +28,7 @@ if morphology1:
     hasil = img_2d_scaled
     st.image(hasil, caption='Gambar Origin',use_column_width=True)
     
+elif otsuthreshold:
     #OTSU THRESHOLDING
     _,binarized = cv2.threshold(hasil, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     foreground_value = 255
@@ -38,140 +39,54 @@ if morphology1:
     binarized[labels == largest_label] = foreground_value
     st.image(binarized, caption='Otsu Image',use_column_width=True)
 
-    # erosion from otsu
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-    erosion = cv2.erode(binarized,kernel,iterations = 4)
-    foreground_value = 255
-    mask = np.uint8(erosion == foreground_value)
-    labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
-    largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
-    erosion = np.zeros_like(erosion)
-    erosion[labels == largest_label] = foreground_value
-    st.image(erosion, caption='Erosion Image',use_column_width=True)
+#     # erosion from otsu
+#     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+#     erosion = cv2.erode(binarized,kernel,iterations = 4)
+#     foreground_value = 255
+#     mask = np.uint8(erosion == foreground_value)
+#     labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
+#     largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
+#     erosion = np.zeros_like(erosion)
+#     erosion[labels == largest_label] = foreground_value
+#     st.image(erosion, caption='Erosion Image',use_column_width=True)
 
-    # dilation from opening
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-    dilasi = cv2.dilate(erosion,kernel,iterations = 2)
-    foreground_value = 255
-    mask = np.uint8(dilasi == foreground_value)
-    labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
-    largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
-    dilasi = np.zeros_like(dilasi)
-    dilasi[labels == largest_label] = foreground_value
-    st.image(dilasi, caption='Dilation Image',use_column_width=True)
+#     # dilation from opening
+#     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+#     dilasi = cv2.dilate(erosion,kernel,iterations = 2)
+#     foreground_value = 255
+#     mask = np.uint8(dilasi == foreground_value)
+#     labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
+#     largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
+#     dilasi = np.zeros_like(dilasi)
+#     dilasi[labels == largest_label] = foreground_value
+#     st.image(dilasi, caption='Dilation Image',use_column_width=True)
 
-    img_2d = file.astype(float)
-    img_2d_scaled = (np.maximum(img_2d,0) / img_2d.max()) * 255.0
-    img_2d_scaled = np.uint8(img_2d_scaled)
-    hasil = img_2d_scaled
+#     img_2d = file.astype(float)
+#     img_2d_scaled = (np.maximum(img_2d,0) / img_2d.max()) * 255.0
+#     img_2d_scaled = np.uint8(img_2d_scaled)
+#     hasil = img_2d_scaled
 
-    #Skull Stripping
-    skull_stripped_image = cv2.bitwise_and(hasil, hasil, mask = dilasi)
-    brain_pixels = skull_stripped_image[dilasi == foreground_value]
+#     #Skull Stripping
+#     skull_stripped_image = cv2.bitwise_and(hasil, hasil, mask = dilasi)
+#     brain_pixels = skull_stripped_image[dilasi == foreground_value]
 
-    # Adapting the data to K-means
-    kmeans_input = np.float32(brain_pixels.reshape(brain_pixels.shape[0], brain_pixels.ndim))
+#     # Adapting the data to K-means
+#     kmeans_input = np.float32(brain_pixels.reshape(brain_pixels.shape[0], brain_pixels.ndim))
 
-    # K-means parameters
-    epsilon = 0.01
-    number_of_iterations = 50
-    number_of_clusters = 4
-    number_of_repetition = 10
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,number_of_iterations, epsilon)
-    flags = cv2.KMEANS_RANDOM_CENTERS
-    # K-means segmentation
-    _, labels, centers = cv2.kmeans(kmeans_input, number_of_clusters, None, criteria,number_of_repetition, flags)
-    # Adapting the labels
-    labels = labels.flatten('F')
-    for x in range(number_of_clusters):
-        labels[labels == x] = centers[x]
-    #segmented Image
-    segmented_image = np.zeros_like(dilasi)
-    segmented_image[dilasi == foreground_value] = labels
-    st.image(segmented_image, caption='Segmented Image',use_column_width=True)
-
-elif morphology2:
-    d = pydicom.read_file('dicom/'+option)
-    file = np.array(d.pixel_array)
-    img = file
-    img_2d = img.astype(float)
-    img_2d_scaled = (np.maximum(img_2d,0) / img_2d.max()) * 255.0
-    img_2d_scaled = np.uint8(img_2d_scaled)
-    hasil = img_2d_scaled
-    st.image(hasil, caption='Gambar Origin',use_column_width=True)
-
-    #OTSU THRESHOLDING
-    _,binarized = cv2.threshold(hasil, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    foreground_value = 255
-    mask = np.uint8(binarized == foreground_value)
-    labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
-    largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
-    binarized = np.zeros_like(binarized)
-    binarized[labels == largest_label] = foreground_value
-    st.image(binarized, caption='Gambar Otsu',use_column_width=True)
-    
-    # Erosion
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-    erosion = cv2.erode(binarized,kernel,iterations = 4)
-    foreground_value = 255
-    mask = np.uint8(erosion == foreground_value)
-    labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
-    largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
-    erosion = np.zeros_like(erosion)
-    erosion[labels == largest_label] = foreground_value
-    st.image(erosion, caption='Gambar Erosion',use_column_width=True)
-    
-    # Opening
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-    opening = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, kernel, iterations= 2)
-    foreground_value = 255
-    mask = np.uint8(opening == foreground_value)
-    labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
-    largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
-    opening = np.zeros_like(opening)
-    opening[labels == largest_label] = foreground_value
-    st.image(opening, caption='Gambar Opening',use_column_width=True)
-    
-    # Dilation
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-    dilasi = cv2.dilate(opening,kernel,iterations = 1)
-    foreground_value = 255
-    mask = np.uint8(dilasi == foreground_value)
-    labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
-    largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
-    dilasi = np.zeros_like(dilasi)
-    dilasi[labels == largest_label] = foreground_value
-    st.image(dilasi, caption='Gambar Dilation',use_column_width=True)
-    
-    img_2d = file.astype(float)
-    img_2d_scaled = (np.maximum(img_2d,0) / img_2d.max()) * 255.0
-    img_2d_scaled = np.uint8(img_2d_scaled)
-    hasil = img_2d_scaled
-
-    #Skull Stripping
-    skull_stripped_image = cv2.bitwise_and(hasil, hasil, mask = dilasi)
-    brain_pixels = skull_stripped_image[dilasi == foreground_value]
-
-    # Adapting the data to K-means
-    kmeans_input = np.float32(brain_pixels.reshape(brain_pixels.shape[0], brain_pixels.ndim))
-
-    # K-means parameters
-    epsilon = 0.01
-    number_of_iterations = 50
-    number_of_clusters = 4
-    number_of_repetition = 10
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,number_of_iterations, epsilon)
-    flags = cv2.KMEANS_RANDOM_CENTERS
-
-    # K-means segmentation
-    _, labels, centers = cv2.kmeans(kmeans_input, number_of_clusters, None, criteria,number_of_repetition, flags)
-
-    # Adapting the labels
-    labels = labels.flatten('F')
-    for x in range(number_of_clusters):
-        labels[labels == x] = centers[x]
-
-    #segmented Image
-    segmented_image = np.zeros_like(dilasi)
-    segmented_image[dilasi == foreground_value] = labels
-    st.image(segmented_image, caption='Gambar Segmented',use_column_width=True)
+#     # K-means parameters
+#     epsilon = 0.01
+#     number_of_iterations = 50
+#     number_of_clusters = 4
+#     number_of_repetition = 10
+#     criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,number_of_iterations, epsilon)
+#     flags = cv2.KMEANS_RANDOM_CENTERS
+#     # K-means segmentation
+#     _, labels, centers = cv2.kmeans(kmeans_input, number_of_clusters, None, criteria,number_of_repetition, flags)
+#     # Adapting the labels
+#     labels = labels.flatten('F')
+#     for x in range(number_of_clusters):
+#         labels[labels == x] = centers[x]
+#     #segmented Image
+#     segmented_image = np.zeros_like(dilasi)
+#     segmented_image[dilasi == foreground_value] = labels
+#     st.image(segmented_image, caption='Segmented Image',use_column_width=True)
