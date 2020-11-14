6854b357e0a06befe76c4bd932d1cc42b42fe461
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-14 09:11:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ IMAGE_PATHS = os.listdir("dicom")
 option = st.sidebar.selectbox('Pilih File Dicom?',IMAGE_PATHS)
 st.sidebar.write('You selected:', option)
 
-def bukadata(option):    
+def bukadata(file):    
     # get the data
-    d = pydicom.read_file('dicom/'+option)
+    d = pydicom.read_file('dicom/'+file)
     file = np.array(d.pixel_array)
     img = file
     img_2d = img.astype(float)
@@ -28,18 +28,20 @@ def bukadata(option):
     st.image(hasil, caption='Gambar Origin')
     return hasil
 
-bukadata(option)
-    
-#     #OTSU THRESHOLDING
-#     _,binarized = cv2.threshold(hasil, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-#     foreground_value = 255
-#     mask = np.uint8(binarized == foreground_value)
-#     labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
-#     largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
-#     binarized = np.zeros_like(binarized)
-#     binarized[labels == largest_label] = foreground_value
-#     st.image(binarized, caption='Otsu Image')
+def threshold(image):
+    #OTSU THRESHOLDING
+    _,binarized = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    foreground_value = 255
+    mask = np.uint8(binarized == foreground_value)
+    labels, stats = cv2.connectedComponentsWithStats(mask, 4)[1:3]
+    largest_label = 1 + np.argmax(stats[1:, cv2.CC_STAT_AREA])
+    binarized = np.zeros_like(binarized)
+    binarized[labels == largest_label] = foreground_value
+    st.image(binarized, caption='Otsu Image')
+    return binarized
 
+bukadata(option)
+threshold(hasil)
     
 #     # erosion from otsu
 #     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
