d63229c6e594f82cd692cb8bd64306d0fadbb271
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-15 09:24:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -263,7 +263,7 @@ elif morphology4:
          img_2d_scaled = (np.maximum(img_2d,0) / img_2d.max()) * foreground
          img_2d_scaled = np.uint8(img_2d_scaled)
          hasil = img_2d_scaled
-         [col[num]] = st.beta_columns(len(IMAGE_PATHS))
-         with [col[num]]:
+         col[num] = st.beta_columns(len(IMAGE_PATHS))
+         with col[num]:
               st.image(hasil, use_column_width=True)
          
